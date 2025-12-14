a8a452a80a7573fcf29d8cddd7fd370bec2293e8
Rajagarwal632 <raj390504@gmail.com>
2025-12-14 16:31:11 +0530

    user_auth middleware created

---
+++ middleware/user.js
@@ -0,0 +1,19 @@
+const JWT_USER = process.env.JWT_USER
+const jwt = require("jsonwebtoken")
+
+function userauth(req,res,next){
+    const token = req.headers.token
+    const decoded_data = jwt.verify(token,JWT_USER)
+    if(decoded_data){
+        req.userid = decoded_data.userid
+        next()  
+    }
+    else{
+        res.json({
+            msg : "NOT AUTHORIZED"
+        })
+    }
+}
+module.exports = {
+    userauth
+}
